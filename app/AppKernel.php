5ace977ea747d3f48b0da645b96eaf8f052fb0d9
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2011-08-08 08:38:39 +0300

    added SonataAdminBundle and its dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,10 @@ class AppKernel extends Kernel
             new Stfalcon\Bundle\PageBundle\StfalconPageBundle(),
             new Stfalcon\Bundle\NewsBundle\StfalconNewsBundle(),
             new Stfalcon\Bundle\EventBundle\StfalconEventBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\BluePrintBundle\SonataBluePrintBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
