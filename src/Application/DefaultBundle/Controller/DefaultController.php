ee4446fd663f5e1f1d278b2d985334fdf88da2b3
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2011-08-08 14:06:23 +0300

    refactored layouts

--- src/Application/DefaultBundle/Controller/DefaultController.php
+++ src/Application/DefaultBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="mainpage")
+     * @Route("/", name="homepage")
      * @Template()
      */
     public function indexAction()
---
+++ src/Application/DefaultBundle/Menu/MainMenu.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\DefaultBundle\Menu;
+
+use Knp\Bundle\MenuBundle\Menu;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Router;
+
+class MainMenu extends Menu
+{
+    /**
+     * @param Request $request
+     * @param Router $router
+     */
+    public function __construct(Request $request, Router $router)
+    {
+        parent::__construct();
+
+        $this->setCurrentUri($request->getRequestUri());
+        
+        $this->addChild('О Frameworks Days', $router->generate('page_show', array('slug' => 'about')));
+        $this->addChild('События', $router->generate('events'));
+        $this->addChild('Контактная информация', $router->generate('page_show', array('slug' => 'contacts')));
+        $this->addChild('Партнеры', $router->generate('page_show', array('slug' => 'partners')));
+    }
+}
