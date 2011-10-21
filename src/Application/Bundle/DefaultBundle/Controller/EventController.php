3b433d2cc34017ceade8045c10d30f80e55741b9
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2011-10-22 02:17:24 +0300

    added "my events"

--- src/Application/Bundle/DefaultBundle/Controller/EventController.php
+++ src/Application/Bundle/DefaultBundle/Controller/EventController.php
@@ -3,31 +3,15 @@
 namespace Application\Bundle\DefaultBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Application event controller
  */
 class EventController extends Controller
 {
-
-    /**
-     * List of past and future events
-     *
-     * @Route("/events", name="events")
-     * @Template()
-     * @return array
-     */
-    public function indexAction()
-    {
-        // @todo refact. отдельнымы спискамм активные и прошедние ивенты
-        $events = $this->getDoctrine()->getEntityManager()
-                       ->getRepository('StfalconEventBundle:Event')->findAll();
-
-        return array('events' => $events);
-    }
 
     /**
      * Events slider (block)
--- src/Stfalcon/Bundle/EventBundle/Controller/EventController.php
+++ src/Stfalcon/Bundle/EventBundle/Controller/EventController.php
@@ -2,14 +2,34 @@
 
 namespace Stfalcon\Bundle\EventBundle\Controller;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+
+use Stfalcon\Bundle\EventBundle\Entity\Ticket;
 
 /**
  * Event controller
  */
 class EventController extends BaseController
 {
+    
+    /**
+     * List of past and future events
+     *
+     * @Route("/events", name="events")
+     * @Template()
+     * @return array
+     */
+    public function indexAction()
+    {
+        // @todo refact. отдельнымы спискамм активные и прошедние ивенты
+        $events = $this->getDoctrine()->getEntityManager()
+                       ->getRepository('StfalconEventBundle:Event')->findAll();
+
+        return array('events' => $events);
+    }
 
     /**
      * Finds and displays a Event entity.
@@ -23,5 +43,69 @@ class EventController extends BaseController
 
         return array('event' => $event);
     }
+
+    /**
+     * Take part in the event. Create new ticket for user
+     *
+     * @Secure(roles="ROLE_USER")
+     * @Route("/event/{event_slug}/take-part", name="event_takePart")
+     * @Template()
+     * @param string $event_slug
+     * @return RedirectResponse
+     */
+    public function takePartAction($event_slug)
+    {
+        $event = $this->getEventBySlug($event_slug);
+
+        // пользователь авторизирован
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        /*
+        // пользователь не авторизирован
+            // я зарегистрированный пользователь
+                // вводит e-mail и пароль
+                // создаем билет
+            // я новый пользователь
+                // <<< регистрируем пользователя
+                // на e-mail отправляем ссылку для активации аккаунта >>>
+                // создаем билет
+        */
+
+        // проверяем или у него нет билетов на этот ивент
+        $em = $this->getDoctrine()->getEntityManager();
+        $ticket = $em->getRepository('StfalconEventBundle:Ticket')->findOneBy(array('event' => $event->getId(), 'user' => $user->getId()));
+
+        // если нет, тогда создаем билет
+        if (is_null($ticket)) {
+            $ticket = new Ticket();
+            $ticket->setUser($user);
+            $ticket->setEvent($event);
+            $ticket->setStatus(Ticket::STATUS_NEW);
+
+            $em->persist($ticket);
+            $em->flush();
+        }
+
+        // переносим на страницу билетов пользователя к хешу /evenets/my#zend-framework-day-2011
+        return new RedirectResponse($this->generateUrl('events_my') . '#' . $event->getSlug());
+    }
+
+    /**
+     * Show user events
+     *
+     * @Secure(roles="ROLE_USER")
+     * @Route("/events/my", name="events_my")
+     * @Template()
+     * @return array
+     */
+    public function myAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $tickets = $em->getRepository('StfalconEventBundle:Ticket')->findBy(array('user' => $user->getId()));
+
+        return array('tickets' => $tickets);
+    }    
 
 }
