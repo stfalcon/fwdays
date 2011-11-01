2c4fc5a689cbec0225edf1ffb17c7c3f8dd19898
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2011-11-02 01:32:12 +0300

    fixed migration

--- app/DoctrineMigrations/Version20111024220034.php
+++ app/DoctrineMigrations/Version20111024220034.php
@@ -16,13 +16,13 @@ class Version20111024220034 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
         
         $this->addSql("ALTER TABLE event__events ADD city VARCHAR(255) DEFAULT NULL, ADD place VARCHAR(255) DEFAULT NULL, ADD date DATETIME DEFAULT NULL, DROP title, CHANGE about about LONGTEXT DEFAULT NULL");
-        $this->addSql("DROP INDEX UNIQ_66E2955971F7E88B ON event__tickets");
-        $this->addSql("DROP INDEX UNIQ_66E295594C3A3BB ON event__tickets");
-        $this->addSql("DROP INDEX UNIQ_66E29559A76ED395 ON event__tickets");
         $this->addSql("ALTER TABLE event__tickets ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL, DROP status");
         $this->addSql("CREATE INDEX IDX_66E2955971F7E88B ON event__tickets (event_id)");
         $this->addSql("CREATE INDEX IDX_66E29559A76ED395 ON event__tickets (user_id)");
         $this->addSql("CREATE INDEX IDX_66E295594C3A3BB ON event__tickets (payment_id)");
+        $this->addSql("DROP INDEX UNIQ_66E2955971F7E88B ON event__tickets");
+        $this->addSql("DROP INDEX UNIQ_66E295594C3A3BB ON event__tickets");
+        $this->addSql("DROP INDEX UNIQ_66E29559A76ED395 ON event__tickets");
         $this->addSql("ALTER TABLE payments ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL, CHANGE user_id user_id INT DEFAULT NULL, CHANGE sum amount NUMERIC(10, 2) NOT NULL");
         $this->addSql("ALTER TABLE payments ADD CONSTRAINT FK_65D29B32A76ED395 FOREIGN KEY (user_id) REFERENCES users(id)");
         $this->addSql("CREATE INDEX IDX_65D29B32A76ED395 ON payments (user_id)");
