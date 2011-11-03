d7cc5ac7911a4bc7f926a1621c22ff34092b304e
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2011-11-03 20:41:06 +0200

    hard code :\

--- src/Application/Bundle/DefaultBundle/Controller/DefaultController.php
+++ src/Application/Bundle/DefaultBundle/Controller/DefaultController.php
@@ -9,16 +9,16 @@ use JMS\SecurityExtraBundle\Annotation\Secure;
 
 use Stfalcon\Bundle\EventBundle\Entity\Event;
 use Stfalcon\Bundle\EventBundle\Entity\Ticket;
+use Stfalcon\Bundle\EventBundle\Entity\Mail;
 use Stfalcon\Bundle\PaymentBundle\Entity\Payment;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      * @Template()
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         return array();
     }
 
@@ -27,58 +27,103 @@ class DefaultController extends Controller
      * @Secure(roles="ROLE_ADMIN")
      * @Template()
      */
-    public function addUsersAction(Event $event)
-    {
-//        $data = $_POST['users'];
-        // @todo разбить на строки и на значения
-        $data = array(array('name' => 'Stepan Tanasiychuk', 'email' => 'stepan.tanasiychuk@gmail.com'),
-            array('name' => 'Victor Paladiychuk', 'email' => 'victor_gugo@stfalcon.com'));
+    public function addUsersAction(Event $event) {
+    // @todo удалить этот метод. одноразовый код
+        if (isset($_POST['users'])) {
+            $data = explode("\r\n", $_POST['users']);
 
-        foreach ($data as $d) {
-            $user = $this->get('fos_user.user_manager')->findUserBy(array('email' => $d['email']));
+            $mail = new Mail();
+            $mail->setTitle('Подтверждение участия в конференции ' . $event->getName());
+            $mail->setText('Доброго времени суток, %fullname%.
 
-            if ($user) {
-                // проверяем или у него нет билетов на этот ивент
-                $em = $this->getDoctrine()->getEntityManager();
+Благодарим Вас, за оплату участия в конференции Zend Framework Day.
+Напоминаем, что конференция состоится 12 ноября 2011 года, в конференц
+зале отеля "Казацкий" (г. Киев, ул. Михайловская 1/3, рядом с Площадью
+Независимости).
 
-                $ticket = $em->getRepository('StfalconEventBundle:Ticket')
-                    ->findOneBy(array('event' => $event->getId(), 'user' => $user->getId()));
-            } else {
-                $user = $this->get('fos_user.user_manager')->createUser();
-                $user->setEmail($d['email']);
-                $user->setFullname($d['name']);
-                $user->setPassword(substr(str_shuffle(md5(time())), 5, 8));
+Регистрация участников начнется в 10 часов утра, а на 11:00
+запланировано начало первого доклада. При себе необходимо
+иметь любой документ удостоверяющий Вашу личность (паспорт,
+водительское удостоверение, студенческий билет).
 
-                $user->setEnabled(false);
-                $this->get('fos_user.mailer')->sendConfirmationEmailMessage($user);
-                $this->get('fos_user.user_manager')->updateUser($user);
-                
-                $ticket = null;
-            }
-            
-            if (!$ticket) {
-                $ticket = new Ticket($event, $user);
-                $em->persist($ticket);
-//                $em->flush();
-            }
-            
-            if ($ticket->isPaid()) {
-                echo "#{$user->getId()} {$user->getFullname()} уже оплатил участие в конференции!!<br>";
-            } else {
-                if (!($payment = $ticket->getPayment())) {
-                    $payment = new Payment($user, 150);
-                    $em->persist($payment);
-                    $ticket->setPayment($payment);
-                    $em->persist($ticket);
-//                    $em->flush();
+До встречи на конференции!
+
+---
+С уважением,
+Орг. Комитет "Zend Framework Day"');
+
+            foreach ($data as $d) {
+                $dt = explode(' ', $d);
+                unset($d);
+                $d['name'] = $dt[0] . ' ' . $dt[1];
+                $d['email'] = $dt[2];
+
+                $user = $this->get('fos_user.user_manager')->findUserBy(array('email' => $d['email']));
+
+                if ($user) {
+                    // проверяем или у него нет билетов на этот ивент
+                    $em = $this->getDoctrine()->getEntityManager();
+
+                    $ticket = $em->getRepository('StfalconEventBundle:Ticket')
+                            ->findOneBy(array('event' => $event->getId(), 'user' => $user->getId()));
+                    echo "#{$user->getId()} {$user->getFullname()} уже зарегистрирован ---<br>";
+                } else {
+                    $user = $this->get('fos_user.user_manager')->createUser();
+                    $user->setEmail($d['email']);
+                    $user->setFullname($d['name']);
+                    $user->setPlainPassword(substr(str_shuffle(md5(time())), 5, 8));
+
+                    $user->setEnabled(false);
+                    $this->get('fos_user.mailer')->sendConfirmationEmailMessage($user);
+                    $this->get('fos_user.user_manager')->updateUser($user);
+
+                    $ticket = null;
                 }
-                $payment->setGate('admin');
-                $payment->setStatus('paid');
-                $em->persist($payment);
-                $em->flush();
+
+                if (!$ticket) {
+                    $ticket = new Ticket($event, $user);
+                    $em->persist($ticket);
+                    //                $em->flush();
+                }
+
+                if ($ticket->isPaid()) {
+                    echo "#{$user->getId()} {$user->getFullname()} уже оплатил участие в конференции!!<br>";
+                } else {
+                    if (!($payment = $ticket->getPayment())) {
+                        $payment = new Payment($user, 150);
+                        $em->persist($payment);
+                        $ticket->setPayment($payment);
+                        $em->persist($ticket);
+                        //                    $em->flush();
+                    }
+                    $payment->setGate('admin');
+                    $payment->setStatus('paid');
+                    $em->persist($payment);
+                    $em->flush();
+
+                    $text = $mail->replace(
+                        array(
+                            '%fullname%' => $user->getFullname(),
+                            '%user_id%' => $user->getId(),
+                        )
+                    );
+
+                    $message = \Swift_Message::newInstance()
+                        ->setSubject($mail->getTitle())
+                        // @todo refact
+                        ->setFrom('orgs@fwdays.com', 'Frameworks Days')
+                        ->setTo($user->getEmail())
+                        ->setBody($text);
+
+                    // @todo каждый вызов отнимает память
+                    $this->get('mailer')->send($message);
+
+                }
             }
-        }
+
+            echo 'complete';
             exit;
+        }
 
         return array();
     }
