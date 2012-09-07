1b5c11601ccee3eb0197aea80b26273de337fdfc
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2012-09-08 01:29:58 +0300

    added git_enable_submodules to deploy config

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -6,6 +6,7 @@ set :app_path,    "app"
 
 set :repository,  "git://github.com/stfalcon/fwdays.git"
 set :scm,         :git
+set :git_enable_submodules, 1
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
@@ -13,11 +14,11 @@ role :db,         domain, :primary => true       # This is where Rails migration
 
 default_run_options[:pty] = true
 
-set  :keep_releases,  3
-set  :user,       "fwdays-com"
-set  :use_sudo,   false
+set :keep_releases,  3
+set :user,       "fwdays-com"
+set :use_sudo,   false
 
-set  :update_vendors, true
+set :update_vendors, true
 set :deploy_via, :rsync_with_remote_cache
 
 set :shared_files,	["app/config/parameters.ini"]
