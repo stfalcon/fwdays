7f995c86d71c995c8c58b3a30ed0048a962eff55
jeka <jeka@stfalcon.com>
2012-09-11 13:16:40 +0300

    fix capifony config to use composer

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -17,7 +17,9 @@ set  :keep_releases,  3
 set  :user,       "fwdays-com"
 set  :use_sudo,   false
 
-set  :update_vendors, true
+#set  :update_vendors, true
+set :use_composer, true
+
 set :deploy_via, :rsync_with_remote_cache
 
 set :shared_files,	["app/config/parameters.ini"]
