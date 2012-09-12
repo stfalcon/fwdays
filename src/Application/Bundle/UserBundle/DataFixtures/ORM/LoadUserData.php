9b97fb6b351ce665d100c8d54fd4dc71ea233089
Igor Khokhlov <eld2303@gmail.com>
2012-09-12 14:32:02 +0300

    fix migrations, submod version, add user fixtures

---
+++ src/Application/Bundle/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Application\Bundle\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Application\Bundle\UserBundle\Entity\User;
+
+/**
+ * Users fixtures
+ */
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * Create and load user fixtures to database
+     *
+     * @param ObjectManager $manager Entity manager object
+     *
+     * @return void
+     */
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('Администратор');
+        $userAdmin->setEmail('admin@fwdays.com');
+        $userAdmin->setPlainPassword('qwerty');
+        $userAdmin->setRoles(array('ROLE_SUPER_ADMIN'));
+        $userAdmin->setEnabled(true);
+        $userAdmin->setExpired(false);
+        $userAdmin->setLocked(false);
+        $manager->persist($userAdmin);
+        $manager->flush();
+        $this->addReference('user-admin', $userAdmin);
+
+        $userDefault = new User();
+        $userDefault->setUsername('Пользователь');
+        $userDefault->setEmail('user@fwdays.com');
+        $userDefault->setPlainPassword('qwerty');
+        $userDefault->addRole('ROLE_USER');
+        $userDefault->setEnabled(true);
+        $userDefault->setExpired(false);
+        $userDefault->setLocked(false);
+        $manager->persist($userDefault);
+        $manager->flush();
+        $this->addReference('user-default', $userDefault);
+
+
+    }
+
+    /**
+     * Get the number for sorting fixture
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return 1; // the order in which fixtures will be loaded
+    }
+
+}
