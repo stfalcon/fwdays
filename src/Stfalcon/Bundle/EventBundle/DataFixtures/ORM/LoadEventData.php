3f37d4dc50a358b380788c13ad41c8a12a9ecf5d
Artem Genvald <genvaldartem@gmail.com>
2012-09-14 11:32:53 +0300

    Update fixtures

--- src/Stfalcon/Bundle/EventBundle/DataFixtures/ORM/LoadEventData.php
+++ src/Stfalcon/Bundle/EventBundle/DataFixtures/ORM/LoadEventData.php
@@ -8,8 +8,14 @@ use Doctrine\Common\DataFixtures\AbstractFixture,
 
 use Stfalcon\Bundle\EventBundle\Entity\Event;
 
+/**
+ * LoadEventData Class
+ */
 class LoadEventData extends AbstractFixture implements OrderedFixtureInterface
 {
+    /**
+     * @param \Doctrine\Common\Persistence\ObjectManager $manager
+     */
     public function load(ObjectManager $manager)
     {
         $event = new Event();
@@ -23,11 +29,29 @@ class LoadEventData extends AbstractFixture implements OrderedFixtureInterface
         $event->setDate(new \DateTime("2012-04-19", new \DateTimeZone('Europe/Kiev')));
 
         $manager->persist($event);
-        $manager->flush();
-
         $this->addReference('event-zfday', $event);
+
+        unset($event);
+
+        $event = new Event();
+        $event->setName('PHP Frameworks Day');
+        $event->setSlug('php-frameworks-day-2012');
+        $event->setDescription('PHP frameworks day это конференция по современным PHP фреймворкам (Zend Framework 2, Symfony 2, Silex, Lithium и др.)');
+        $event->setLogo('/tmp/logo.jpg');
+        $event->setCity('Киев');
+        $event->setPlace('Пока неизвестно');
+        $event->setAbout("Описание события");
+        $event->setDate(new \DateTime("2012-11-18", new \DateTimeZone('Europe/Kiev')));
+
+        $manager->persist($event);
+        $this->addReference('event-phpday', $event);
+
+        $manager->flush();
     }
 
+    /**
+     * @return int
+     */
     public function getOrder()
     {
         return 1; // the order in which fixtures will be loaded
