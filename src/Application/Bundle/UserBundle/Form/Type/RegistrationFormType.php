853a0e0baa7a8f6fa3a0b5e9d87fb733fa96585b
Artem Genvald <genvaldartem@gmail.com>
2012-09-24 14:35:41 +0300

    Add new markup and fields to registration form

--- src/Application/Bundle/UserBundle/Form/Type/RegistrationFormType.php
+++ src/Application/Bundle/UserBundle/Form/Type/RegistrationFormType.php
@@ -5,6 +5,9 @@ namespace Application\Bundle\UserBundle\Form\Type;
 use FOS\UserBundle\Form\Type\RegistrationFormType as BaseRegistrationFormType;
 use Symfony\Component\Form\FormBuilderInterface;
 
+/**
+ * RegistrationFormType Class
+ */
 class RegistrationFormType extends BaseRegistrationFormType
 {
     /**
@@ -14,18 +17,38 @@ class RegistrationFormType extends BaseRegistrationFormType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', 'email', array('label' => 'fos_user_profile_form_email', 'translation_domain' => 'FOSUserBundle'))
-            ->add('fullname', null, array('label' => 'fos_user_profile_form_fullname', 'translation_domain' => 'FOSUserBundle'))
-            ->add('plainPassword', 'repeated', array(
-                'type' => 'password',
-                'options' => array('translation_domain' => 'FOSUserBundle'),
-                'first_options' => array('label' => 'form.password'),
-                'second_options' => array('label' => 'form.password_confirmation'),
-                )
-            )
-            ->add('company', null, array('required' => false, 'label' => 'fos_user_registration_form_company', 'translation_domain' => 'FOSUserBundle'))
-            ->add('post', null, array('required' => false, 'label' => 'fos_user_profile_form_post', 'translation_domain' => 'FOSUserBundle'))
-            ->add('subscribe', 'checkbox', array('required' => false, 'label' => 'fos_user_profile_form_subscribe', 'translation_domain' => 'FOSUserBundle'));
+            ->add('email', 'email', array(
+                'label'              => 'fos_user_profile_form_email',
+                'translation_domain' => 'FOSUserBundle'
+            ))
+            ->add('fullname', null, array(
+                'required'           => true,
+                'label'              => 'fos_user_profile_form_fullname',
+                'translation_domain' => 'FOSUserBundle'
+            ))
+            ->add('country', null, array(
+                'label'              => 'fos_user_profile_form_country',
+                'translation_domain' => 'FOSUserBundle'
+            ))
+            ->add('city', null, array(
+                'label'              => 'fos_user_profile_form_city',
+                'translation_domain' => 'FOSUserBundle'
+            ))
+            ->add('company', null, array(
+                'required'           => false,
+                'label'              => 'fos_user_registration_form_company',
+                'translation_domain' => 'FOSUserBundle'
+            ))
+            ->add('post', null, array(
+                'required'           => false,
+                'label'              => 'fos_user_profile_form_post',
+                'translation_domain' => 'FOSUserBundle'
+            ))
+            ->add('subscribe', 'checkbox', array(
+                'required'           => false,
+                'label'              => 'fos_user_profile_form_subscribe',
+                'translation_domain' => 'FOSUserBundle'
+            ));
     }
 
     /**
