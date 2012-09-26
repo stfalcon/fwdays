61e2f6f88aa73fba04e110c8705a54c8c5a1490a
Artem Genvald <genvaldartem@gmail.com>
2012-09-26 10:01:17 +0300

    Fix Event fixtures

--- src/Stfalcon/Bundle/EventBundle/DataFixtures/ORM/LoadEventData.php
+++ src/Stfalcon/Bundle/EventBundle/DataFixtures/ORM/LoadEventData.php
@@ -22,7 +22,7 @@ class LoadEventData extends AbstractFixture implements OrderedFixtureInterface
         $event->setName('Zend Framework Day');
         $event->setSlug('zend-framework-day-2011');
         $event->setDescription('Zend Framework Day посвящен популярному PHP фреймворку Zend Framework и является наследником конференции ZFConf Ukraine 2010.');
-        $event->setLogo('/tmp/logo.jpg');
+        $event->setLogo('/bundles/stfalconevent/images/zend-framework-day.png');
         $event->setCity('Киев');
         $event->setPlace('отель "Казацкий"');
         $event->setAbout("Описание события");
@@ -38,7 +38,7 @@ class LoadEventData extends AbstractFixture implements OrderedFixtureInterface
         $event->setName('PHP Frameworks Day');
         $event->setSlug('php-frameworks-day-2012');
         $event->setDescription('PHP frameworks day это конференция по современным PHP фреймворкам (Zend Framework 2, Symfony 2, Silex, Lithium и др.)');
-        $event->setLogo('/tmp/logo.jpg');
+        $event->setLogo('/bundles/stfalconevent/images/php-frameworks-day-2012.png');
         $event->setCity('Киев');
         $event->setPlace('Пока неизвестно');
         $event->setAbout("Описание события");
@@ -53,7 +53,7 @@ class LoadEventData extends AbstractFixture implements OrderedFixtureInterface
         $event->setName('Not Active Frameworks Day');
         $event->setSlug('not-active-frameworks-day');
         $event->setDescription('Это событие тестовое, но должно быть неактивным');
-        $event->setLogo('/tmp/logo.jpg');
+        $event->setLogo('/bundles/stfalconevent/images/smile-lol-icon.png');
         $event->setCity('Где-то там');
         $event->setPlace('Пока неизвестно');
         $event->setAbout("Описание события");
