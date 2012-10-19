5bd93209bed7b6384ab109246d1877d604d00c31
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2012-10-19 17:35:27 +0300

    added JMS\DiExtraBundle to AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
 
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
