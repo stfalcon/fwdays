4a9a806f3741b64c2e0a864e1f4df4796d61b101
Roman Radnuk <webkmua@gmail.com>
2012-10-24 10:51:00 +0300

    Added category to sponsors

---
+++ src/Stfalcon/Bundle/SponsorBundle/Entity/Category.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Stfalcon\Bundle\SponsorBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Stfalcon\Bundle\SponsorBundle\Entity\Category
+ *
+ * @ORM\Table(name="sponsors_category")
+ * @ORM\Entity(repositoryClass="Stfalcon\Bundle\SponsorBundle\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
