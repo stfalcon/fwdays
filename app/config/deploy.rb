faafc5137bda13410058588f2a124084b3ac671f
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2012-11-15 21:33:05 +0200

    disabled git submodules

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -5,7 +5,6 @@ set :app_path,    "app"
 
 set :repository,  "git://github.com/stfalcon/fwdays.git"
 set :scm,         :git
-set :git_enable_submodules, 1
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
@@ -13,13 +12,14 @@ role :db,         domain, :primary => true       # This is where Rails migration
 
 default_run_options[:pty] = true
 
-set :keep_releases,  3
-set :user,       "fwdays-com"
-set :use_sudo,   false
+set :keep_releases, 3
+set :user,  "fwdays-com"
+set :use_sudo,  false
 
-set :use_composer, true
-set :update_vendors, true
+set :use_composer,  true
+#set :copy_vendors,  true
+#set :update_vendors,    true
 
-set :shared_files,	["app/config/parameters.ini"]
-set :shared_children,	[app_path + "/logs", web_path + "/uploads"]
-set :dump_assetic_assets, true
+set :shared_files,  ["app/config/parameters.ini"]
+set :shared_children,   [app_path + "/logs", web_path + "/uploads"]
+set :dump_assetic_assets,   true
