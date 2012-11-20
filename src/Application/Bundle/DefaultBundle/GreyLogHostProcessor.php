8107db81449faad8b49c0f08d50e96cb135449e1
Roman Radnuk <webkmua@gmail.com>
2012-11-20 14:07:32 +0200

    names and docblock change

--- src/Application/Bundle/DefaultBundle/GreyLogHostProcessor.php
+++ src/Application/Bundle/DefaultBundle/GreyLogHostProcessor.php
@@ -1,16 +1,14 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: user
- * Date: 11/20/12
- * Time: 1:25 PM
- * To change this template use File | Settings | File Templates.
+ * Adding HTTP_HOST to the log messages
+ * @param array $record
+ *
+ * @return array
  */
 namespace Application\Bundle\DefaultBundle;
 
 class GreyLogHostProcessor
 {
-
     public function processRecord(array $record)
     {
         $record['extra']['host'] = $_SERVER['HTTP_HOST'];
