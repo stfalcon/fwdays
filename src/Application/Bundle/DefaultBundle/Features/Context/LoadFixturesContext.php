0bb1d27057acedc9a3e10207fd0df14b0cf7eff4
Roman Radnuk <webkmua@gmail.com>
2012-11-30 16:45:38 +0200

    Create Base LoadFixturesContext

---
+++ src/Application/Bundle/DefaultBundle/Features/Context/LoadFixturesContext.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Application\Bundle\DefaultBundle\Features\Context;
+
+use Behat\Behat\Context\BehatContext;
+/**
+ * Provides some steps/methods which are useful for recursive loading Depended Fixtures
+ */
+class LoadFixturesContext extends BehatContext
+{
+    /**
+     * Load a data fixture class.
+     *
+     * @param \Symfony\Bundle\DoctrineFixturesBundle\Common\DataFixtures\Loader $loader
+     * @param string $className or array
+     */
+    public function loadFixtureClass($loader, $className)
+    {
+
+        if (is_array($className)) {
+            foreach ($className as $class) {
+                $this->loadFixtureClass($loader, $class);
+            }
+        } else {
+
+            $fixture = new $className();
+
+            if ($loader->hasFixture($fixture)) {
+                unset($fixture);
+                return;
+            }
+
+            $loader->addFixture($fixture);
+
+            if ($fixture instanceof DependentFixtureInterface) {
+                foreach ($fixture->getDependencies() as $dependency) {
+                    $this->loadFixtureClass($loader, $dependency);
+                }
+            }
+        }
+    }
+
+}
