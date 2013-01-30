f47af7776922c8b1a0993232d99b7eac703b6015
Mutuh <sirmutuh@gmail.com>
2013-01-31 00:37:14 +0200

    image edit templates fix

--- src/Stfalcon/Bundle/EventBundle/Entity/Speaker.php
+++ src/Stfalcon/Bundle/EventBundle/Entity/Speaker.php
@@ -64,7 +64,7 @@ class Speaker
     /**
      * @var string $photo
      *
-     * @ORM\Column(name="photo", type="string", length=255)
+     * @ORM\Column(name="photo", type="string", length=255, nullable=true)
      */
     private $photo;
 
