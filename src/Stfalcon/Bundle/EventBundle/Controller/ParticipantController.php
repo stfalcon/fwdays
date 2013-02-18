50b747d5ed7a50595bcb4c35050e2683c174ce0a
Mutuh <sirmutuh@gmail.com>
2013-02-18 15:13:31 +0200

    feature participants page

--- src/Stfalcon/Bundle/EventBundle/Controller/ParticipantController.php
+++ src/Stfalcon/Bundle/EventBundle/Controller/ParticipantController.php
@@ -14,6 +14,31 @@ use Stfalcon\Bundle\EventBundle\Entity\Event;
 class ParticipantController extends BaseController
 {
     /**
+     * Lists all speakers for event
+     *
+     * @param string $event_slug
+     *
+     * @return array
+     *
+     * @Route("/event/{event_slug}/participants", name="event_participants")
+     * @Template()
+     */
+    public function indexAction($event_slug)
+    {
+        $event = $this->getEventBySlug($event_slug);
+
+        /** @var $ticketRepository \Stfalcon\Bundle\EventBundle\Repository\TicketRepository */
+        $ticketRepository = $this->getDoctrine()->getManager()->getRepository('StfalconEventBundle:Ticket');
+
+        $participants = $ticketRepository->findTicketsByEventGroupByUser($event);
+
+        return array(
+            'event' => $event,
+            'participants' => $participants
+        );
+    }
+
+    /**
      * List of participants for event
      *
      * @param Event $event Event
