46967942c209f4764adcfb4e83658a7530aa3693
Mutuh <sirmutuh@gmail.com>
2013-02-26 17:36:29 +0200

    Updating fixtures

--- app/DoctrineMigrations/Version20130226173124.php
+++ app/DoctrineMigrations/Version20130226173124.php
@@ -8,21 +8,21 @@ use Doctrine\DBAL\Migrations\AbstractMigration,
 /**
  * Auto-generated Migration: Please modify to your need!
  */
-class Version20130221160130 extends AbstractMigration
+class Version20130226173124 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
-
-        $this->addSql("ALTER TABLE event__events CHANGE backgroundimage background_image VARCHAR(255) NOT NULL");
+        
+        $this->addSql("ALTER TABLE event__events ADD background_image VARCHAR(255) NOT NULL");
     }
 
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
-
-        $this->addSql("ALTER TABLE event__events CHANGE background_image backgroundImage VARCHAR(255) NOT NULL");
+        
+        $this->addSql("ALTER TABLE event__events DROP background_image");
     }
 }
