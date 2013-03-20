a46e84707bc3be0b433b8798d95c116f82d81fe7
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2013-03-20 17:09:09 +0200

    changed simple-slider logic

--- web/js/simple-slider.js
+++ web/js/simple-slider.js
@@ -2,11 +2,27 @@ $(document).ready(function () {
     var count = $("div.event-slider div.slide").length;
     var currentSlide = 0;
     var enabledOpacity = 1;
-    var removedOpacity = 0.4;
+    var removedOpacity = 0.1;
     var animationInterval = 5000;
     var isAutomatic = true;
 
-    $('.btn').click(function () {
+    // automatic scrolling slide
+    window.setInterval(function () {
+        if (!isAutomatic) {
+            return;
+        }
+
+        if (currentSlide < count - 1) {
+            currentSlide++;
+        } else {
+            currentSlide = 0;
+        }
+
+        changeSlide();
+    }, animationInterval);
+
+    // click on next/prev slide button
+    $('.event-slider .btn').click(function () {
         isAutomatic = false;
 
         if ($(this).hasClass("btn-next")) {
@@ -25,43 +41,31 @@ $(document).ready(function () {
             }
         }
 
-        slide();
+        changeSlide();
     });
 
     $('div#wrapper div.events-switches ul li').click(function (event) {
         isAutomatic = false;
-        var index = $(this).index();
-        currentSlide = index;
+        currentSlide = $(this).index();
 
-        slide();
+        changeSlide();
     });
 
-    window.setInterval(automatic, animationInterval);
+    function changeSlide() {
+        $("div.event-slider div.slide")
+            .animate({ opacity: removedOpacity }, "fast", function() {
+                // hide all slides
+                $("div.event-slider div.slide").hide();
 
-    function automatic() {
-        if (!isAutomatic) {
-            return;
-        }
+                // change active link in nav switcher
+                $("div.events-switches div.wrap ul li").removeClass("active");
+                $("div#wrapper div.events-switches ul li:eq(" + currentSlide + ")").addClass('active');
 
-        if (currentSlide < count - 1) {
-            currentSlide++;
-        } else {
-            currentSlide = 0;
-        }
-
-        slide();
-    }
-
-    function slide() {
-        // hide current slide
-        $("div.event-slider div.slide").hide()
-            .animate({ opacity: removedOpacity }, "slow");
-        // show next slide
-        $("div.event-slider div.slide:eq(" + currentSlide + ")").show()
-            .animate({ opacity: enabledOpacity }, "slow");
-
-        // change active link in nav switcher
-        $("div.events-switches div.wrap ul li").removeClass("active");
-        $("div#wrapper div.events-switches ul li:eq(" + currentSlide + ")").addClass('active');
+                // snow next slide
+                $("div.event-slider div.slide:eq(" + currentSlide + ")")
+                    .css({opacity: removedOpacity })
+                    .show()
+                    .animate({ opacity: enabledOpacity }, "fast");
+            });
     }
 });
