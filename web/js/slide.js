fbb7bded0b96ef2e20dd7076179d5f2fa1ab18a3
Artem Genvald <genvaldartem@gmail.com>
2013-08-20 13:32:36 +0300

    Fix styles for slider

--- web/js/slide.js
+++ web/js/slide.js
@@ -27,14 +27,17 @@ var supportsOrientationChange = "onorientationchange" in window,
 function setSlider() {
     $('#header .photo-slider').cycle('destroy');
     $('#header .photo-slider li').attr('style', '');
-    if ($('#header .photo-slider img').height() > $('#header .photo-slider').height())
-        $('#header .photo-slider img').css('top', '-' + ( ($('#header .photo-slider img').height() - $('#header .photo-slider').height()) / 2 ) + 'px')
-    else
+    if ($('#header .photo-slider img').height() > $('#header .photo-slider').height()) {
+        $('#header .photo-slider img').css('top', '-' + ( ($('#header .photo-slider img').height() - $('#header .photo-slider').height()) / 2 ) + 'px');
+    } else {
         $('#header .photo-slider img').css('top', '0');
-    if ($('#header .photo-slider img').width() > $('#header .photo-slider').width())
-        $('#header .photo-slider img').css('left', '-' + ( ($('#header .photo-slider img').width() - $('#header .photo-slider').width()) / 2 ) + 'px')
-    else
+    }
+    if ($('#header .photo-slider img').width() > $('#header .photo-slider').width()) {
+        $('#header .photo-slider img').css('left', '-' + ( ($('#header .photo-slider img').width() - $('#header .photo-slider').width()) / 2 ) + 'px');
+    } else {
         $('#header .photo-slider img').css('left', '0');
+    }
+
     $('#header .photo-slider').cycle({
 //        fx: 'scrollLeft',
         speed:1500,
