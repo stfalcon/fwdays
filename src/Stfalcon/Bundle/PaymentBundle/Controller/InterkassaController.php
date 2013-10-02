784aab4b060228671a1e8c0dea9397d8e32ef54f
Artem Genvald <genvaldartem@gmail.com>
2013-10-02 11:58:55 +0300

    Uncomment commented code

--- src/Stfalcon/Bundle/PaymentBundle/Controller/InterkassaController.php
+++ src/Stfalcon/Bundle/PaymentBundle/Controller/InterkassaController.php
@@ -155,40 +155,40 @@ class InterkassaController extends Controller
      */
     private function _checkPaymentStatus($params)
     {
-//        if (!array_key_exists('ik_shop_id', $params) ||
-//            !array_key_exists('ik_payment_amount', $params) ||
-//            !array_key_exists('ik_payment_id', $params) ||
-//            !array_key_exists('ik_paysystem_alias', $params) ||
-//            !array_key_exists('ik_baggage_fields', $params) ||
-//            !array_key_exists('ik_payment_state', $params) ||
-//            !array_key_exists('ik_trans_id', $params) ||
-//            !array_key_exists('ik_currency_exch', $params) ||
-//            !array_key_exists('ik_fees_payer', $params)) {
-//            return false;
-//        }
-//
-//        $config = $this->container->getParameter('stfalcon_payment.config');
-//
-//        $crc = md5(
-//            $params['ik_shop_id'] . ':' .
-//            $params['ik_payment_amount'] . ':' .
-//            $params['ik_payment_id'] . ':' .
-//            $params['ik_paysystem_alias'] . ':' .
-//            $params['ik_baggage_fields'] . ':' .
-//            $params['ik_payment_state'] . ':' .
-//            $params['ik_trans_id'] . ':' .
-//            $params['ik_currency_exch'] . ':' .
-//            $params['ik_fees_payer'] . ':' .
-//            $config['interkassa']['secret']
-//        );
-//
-//        $paymentIsSuccess = ('success' == $params['ik_payment_state']);
-//
-//        if (strtoupper($params['ik_sign_hash']) === strtoupper($crc) && $paymentIsSuccess) {
+        if (!array_key_exists('ik_shop_id', $params) ||
+            !array_key_exists('ik_payment_amount', $params) ||
+            !array_key_exists('ik_payment_id', $params) ||
+            !array_key_exists('ik_paysystem_alias', $params) ||
+            !array_key_exists('ik_baggage_fields', $params) ||
+            !array_key_exists('ik_payment_state', $params) ||
+            !array_key_exists('ik_trans_id', $params) ||
+            !array_key_exists('ik_currency_exch', $params) ||
+            !array_key_exists('ik_fees_payer', $params)) {
+            return false;
+        }
+
+        $config = $this->container->getParameter('stfalcon_payment.config');
+
+        $crc = md5(
+            $params['ik_shop_id'] . ':' .
+            $params['ik_payment_amount'] . ':' .
+            $params['ik_payment_id'] . ':' .
+            $params['ik_paysystem_alias'] . ':' .
+            $params['ik_baggage_fields'] . ':' .
+            $params['ik_payment_state'] . ':' .
+            $params['ik_trans_id'] . ':' .
+            $params['ik_currency_exch'] . ':' .
+            $params['ik_fees_payer'] . ':' .
+            $config['interkassa']['secret']
+        );
+
+        $paymentIsSuccess = ('success' == $params['ik_payment_state']);
+
+        if (strtoupper($params['ik_sign_hash']) === strtoupper($crc) && $paymentIsSuccess) {
             return true;
-//        } else {
-//            return false;
-//        }
+        } else {
+            return false;
+        }
     }
 
     /**
