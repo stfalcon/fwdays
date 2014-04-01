15399f0209213ed0d05ea6dab23c8ce9ffa159a1
Olexandr Karataev <olexandr.karataev@stfalcon.com>
2014-04-01 10:44:27 +0300

    add dbg

--- src/Stfalcon/Bundle/PaymentBundle/Controller/InterkassaController.php
+++ src/Stfalcon/Bundle/PaymentBundle/Controller/InterkassaController.php
@@ -79,6 +79,11 @@ class InterkassaController extends Controller
                      ->getRepository('StfalconPaymentBundle:Payment')
                      ->findOneBy(array('id' => $request->get('ik_pm_no')));
 
+        var_dump($payment);
+        var_dump($request->get('ik_pm_no'));
+        var_dump($request->get('ik_inv_st'));
+        var_dump($request->get('ik_co_id'));
+        var_dump($request->get('ik_am'));
         if ($payment->getStatus() == Payment::STATUS_PAID) {
             $resultMessage = 'Проверка контрольной подписи данных о платеже успешно пройдена!';
         } else {
