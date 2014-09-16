e0079d81baec561e3aa1007d1671c449b7fe7709
timur <timur.bolotyuh@gmail.com>
2014-09-16 16:44:55 +0300

    fixed double loading

--- src/Stfalcon/Bundle/EventBundle/Resources/public/js/participants-scroll.js
+++ src/Stfalcon/Bundle/EventBundle/Resources/public/js/participants-scroll.js
@@ -1,30 +1,38 @@
 $(document).ready(function (){
     var $participantsWrapper = $('ul.participants');
 
+    lock = false;//fix double content
+
     /** Get preloader element */
     var $preLoader = $('.preloader');
     $(window).scroll(function () {
 
         /** Get ul.participants bottom offset */
         var participantsWrapperOffsetBottom = ($participantsWrapper.offset().top + $participantsWrapper.height()) - $(window).height();
-        if ($(window).scrollTop() >= (participantsWrapperOffsetBottom)) {
+        if ($(window).scrollTop() >= (participantsWrapperOffsetBottom) && lock===false) {
 
             /** Count of showed participants */
             var offset = $('ul.participants li').length;
-
-            /** Sent AJAX request for get next part of participants */
-            $.ajax({
-                cache: false,
-                async: false,
-                url: window.location.href + '/' + offset,
-                beforeSend: function () {
-                    $preLoader.show();
-                },
-                success: function (data) {
-                    $participantsWrapper.append(data);
-                    $preLoader.hide();
-                }
-            });
+            lock = true;
+            loadContent(offset,$participantsWrapper, $preLoader);
         }
     });
+
+    /** Sent AJAX request for get next part of participants */
+    function loadContent(offset, wrapper, loader) {
+        $.ajax({
+            cache: false,
+            async: false,
+            url: window.location.href + '/' + offset,
+            beforeSend: function () {
+                loader.show();
+            },
+            success: function (data) {
+                wrapper.append(data);
+                loader.hide();
+                lock = false;
+            }
+        });
+    }
+
 })
