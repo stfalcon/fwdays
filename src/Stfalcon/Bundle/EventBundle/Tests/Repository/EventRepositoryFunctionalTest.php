5f495afd1283d6284720f33ea187c976b2e89819
timur <timur.bolotyuh@gmail.com>
2014-09-18 14:17:12 +0300

    use LiipFunctionalTest

--- src/Stfalcon/Bundle/EventBundle/Tests/Repository/EventRepositoryFunctionalTest.php
+++ src/Stfalcon/Bundle/EventBundle/Tests/Repository/EventRepositoryFunctionalTest.php
@@ -2,8 +2,10 @@
 
 namespace Stfalcon\Bundle\EventBundle\Tests\Repository;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Stfalcon\Bundle\EventBundle\Repository\EventRepository;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor,
+    Doctrine\Common\DataFixtures\Purger\ORMPurger;
 
 
 class EventRepositoryFunctionalTest extends WebTestCase
@@ -16,34 +18,55 @@ class EventRepositoryFunctionalTest extends WebTestCase
     /**
      * @var \Doctrine\ORM\EntityManager
      */
-    private $_em;
+    private $em;
 
     public function setUp() {
-        $kernel = static::createKernel();
-        $kernel->boot();
 
-        $this->_em = $kernel->getContainer()
-            ->get('doctrine.orm.entity_manager');
+        static::$kernel = static::createKernel();
+        static::$kernel->boot();
 
-        $this->eventRepository = $this->_em
-            ->getRepository('StfalconEventBundle:Event');
+        $this->em = static::$kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $this->eventRepository = $this->em->getRepository('StfalconEventBundle:Event');
+
+        $connection = $this->em->getConnection();
+
+        $connection->beginTransaction();
+        $connection->query('SET FOREIGN_KEY_CHECKS=0');
+        $connection->commit();
+        $purger   = new ORMPurger();
+        $purger->setPurgeMode(ORMPurger::PURGE_MODE_TRUNCATE);
+        $executor = new ORMExecutor($this->em, $purger);
+        $executor->purge();
+        $connection->beginTransaction();
+        $connection->query('SET FOREIGN_KEY_CHECKS=1');
+        $connection->commit();
+
+        $this->loadFixtures(
+            [
+                '\Stfalcon\Bundle\EventBundle\DataFixtures\ORM\LoadEventData',
+                '\Stfalcon\Bundle\EventBundle\DataFixtures\ORM\LoadTicketData',
+                '\Application\Bundle\UserBundle\DataFixtures\ORM\LoadUserData',
+                '\Stfalcon\Bundle\EventBundle\DataFixtures\ORM\LoadMailQueueData'
+            ]
+        );
+
     }
 
 
     public function testIsActiveEventForUser()
     {
         // пользователь подписан на PHP Frameworks Day
-        $event = $this->_em->getRepository('StfalconEventBundle:Event')->findOneBy(['id' => 6 ]);
-        $user = $this->_em->getRepository('ApplicationUserBundle:User')->findOneBy(['id' => 109]);
+        $event = $this->em->getRepository('StfalconEventBundle:Event')->findOneBy(['id' => 2 ]);
+        $user = $this->em->getRepository('ApplicationUserBundle:User')->findOneBy(['id' => 3]);
         $this->assertTrue($this->eventRepository->isActiveEventForUser($event, $user));
 
         // пользователь подписан на PHP Frameworks Day
-        $user = $this->_em->getRepository('ApplicationUserBundle:User')->findOneBy(['id' => 110]);
+        $user = $this->em->getRepository('ApplicationUserBundle:User')->findOneBy(['id' => 4]);
         $this->assertTrue($this->eventRepository->isActiveEventForUser($event, $user));
 
         // пользователь не подписан на Javascript Frameworks Day
-        $event = $this->_em->getRepository('StfalconEventBundle:Event')->findOneBy(['id' => 8 ]);
-        $user = $this->_em->getRepository('ApplicationUserBundle:User')->findOneBy(['id' => 133]);
+        $event = $this->em->getRepository('StfalconEventBundle:Event')->findOneBy(['id' => 4 ]);
+        $user = $this->em->getRepository('ApplicationUserBundle:User')->findOneBy(['id' => 28]);
         $this->assertFalse($this->eventRepository->isActiveEventForUser($event, $user));
     }
 
