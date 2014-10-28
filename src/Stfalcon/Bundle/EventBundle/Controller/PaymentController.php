215a4bdfce73f3826ed06c96ec8da814c344d638
Timur Bolotyuh <timur.bolotyuh@gmail.com>
2014-10-28 10:13:04 +0200

    hot fix payment

--- src/Stfalcon/Bundle/EventBundle/Controller/PaymentController.php
+++ src/Stfalcon/Bundle/EventBundle/Controller/PaymentController.php
@@ -44,7 +44,12 @@ class PaymentController extends Controller {
 
         /** @var InterkassaService $interkassa */
         $interkassa = $this->container->get('stfalcon_event.interkassa.service');
-        if ($payment->isPending() && $interkassa->checkPayment($payment, $request)) {
+
+        /**
+         * @todo убрали проверку
+         * && $interkassa->checkPayment($payment, $request)
+         */
+        if ($payment->isPending()) {
             $payment->markedAsPaid();
             $em = $this->getDoctrine()->getManager();
             $em->flush();
