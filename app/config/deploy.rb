7c61078adb4d06c3a680b855177879bcbc2294c5
Evgeniy Gurinovich <jeka@geek.km.ua>
2014-10-28 16:40:15 +0200

    deploy to new server

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, "frameworksdays"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
+set :domain,      "144.76.238.50"
+set :deploy_to,   "/var/www/frameworksdays.com"
 set :app_path,    "app"
 
 set :repository,  "git://github.com/stfalcon/fwdays.git"
@@ -11,7 +11,7 @@ role :app,        domain                         # This may be the same as your 
 role :db,         domain, :primary => true       # This is where Rails migrations will run
 
 set :keep_releases, 3
-set :user,  "fwdays-com"
+set :user,  "frameworksdays-com"
 set :use_sudo,  false
 
 set :use_composer,  true
