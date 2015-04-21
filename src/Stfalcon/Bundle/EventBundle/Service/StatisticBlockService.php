5a1503f0692249d5e7c81401c63ca0f8d954b297
Timur Bolotyuh <timur.bolotyuh@gmail.com>
2015-04-21 10:29:23 +0300

    Add service

---
+++ src/Stfalcon/Bundle/EventBundle/Service/StatisticBlockService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Stfalcon\Bundle\EventBundle\Service;
+
+use Sonata\BlockBundle\Block\BaseBlockService;
+use Symfony\Component\HttpFoundation\Response;
+use Sonata\BlockBundle\Block\BlockContextInterface;
+
+/**
+ * StatisticBlockService
+ */
+class StatisticBlockService extends BaseBlockService
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'statistic';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultSettings()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(BlockContextInterface $blockContext, Response $response = null)
+    {
+        return $this->renderResponse(
+            '@StfalconEvent/Statistic/block_admin_list.html.twig',
+            [
+                'block'         => $blockContext->getBlock(),
+                'block_context' => $blockContext
+            ],
+            $response
+        );
+    }
+}
