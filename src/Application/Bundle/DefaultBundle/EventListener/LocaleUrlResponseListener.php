a2fd4708658c391a6fef5ee1f5ea74f4ad068382
Andrew.Tom <andij.tomchyshyn@stfalcon.com>
2018-01-29 15:21:58 +0200

    fix-404

--- src/Application/Bundle/DefaultBundle/EventListener/LocaleUrlResponseListener.php
+++ src/Application/Bundle/DefaultBundle/EventListener/LocaleUrlResponseListener.php
@@ -105,12 +105,12 @@ class LocaleUrlResponseListener
 
         $path = $request->getPathInfo();
         $currentLocal = $this->getInnerSubstring($path, '/');
+        $request->setLocale($currentLocal);
 
         if ($currentLocal === $this->defaultLocale) {
             $params = $request->query->all();
             unset($params[$this->cookieName]);
             $path = ltrim($path, '/'.$currentLocal);
-            $request->setLocale($currentLocal);
             $event->setResponse(new RedirectResponse($request->getBaseUrl().'/'.$path.($params ? '?'.http_build_query($params) : ''), 301));
         }
     }
