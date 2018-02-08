5c8e6065a7b99e71a50a310e2b2ae08e15fa9d3f
Andrew.Tom <andij.tomchyshyn@stfalcon.com>
2018-02-08 09:55:02 +0200

    Refactoring of mailer command

--- src/Stfalcon/Bundle/EventBundle/Entity/Mail.php
+++ src/Stfalcon/Bundle/EventBundle/Entity/Mail.php
@@ -221,6 +221,36 @@ class Mail
     }
 
     /**
+     * @param bool $checkStop
+     *
+     * @return $this
+     */
+    public function incSentMessage($checkStop = true)
+    {
+        ++$this->sentMessages;
+
+        if ($checkStop) {
+            $this->stopIfMailed();
+        }
+
+        return $this;
+    }
+
+    /**
+     * Stop Mail if all mailed
+     *
+     * @return $this
+     */
+    public function stopIfMailed()
+    {
+        if ($this->start) {
+            $this->start = $this->totalMessages === $this->sentMessages;
+        }
+
+        return $this;
+    }
+
+    /**
      * @param int $totalMessages
      */
     public function setTotalMessages($totalMessages)
@@ -234,6 +264,22 @@ class Mail
     public function getTotalMessages()
     {
         return $this->totalMessages;
+    }
+
+    /**
+     * @param bool $checkStop
+     *
+     * @return $this
+     */
+    public function decTotalMessages($checkStop = true)
+    {
+        --$this->totalMessages;
+
+        if ($checkStop) {
+            $this->stopIfMailed();
+        }
+
+        return $this;
     }
 
     /**
