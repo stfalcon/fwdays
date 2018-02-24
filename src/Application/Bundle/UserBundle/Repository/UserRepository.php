e32a9dcca252f74dce91331d8615941678008dc3
Andrew.Tom <andij.tomchyshyn@stfalcon.com>
2018-02-24 12:40:16 +0200

    Change like
    re #39876

--- src/Application/Bundle/UserBundle/Repository/UserRepository.php
+++ src/Application/Bundle/UserBundle/Repository/UserRepository.php
@@ -17,8 +17,7 @@ class UserRepository extends EntityRepository
     public function getAdmins()
     {
         return $this->createQueryBuilder('u')
-            ->where("u.roles LIKE '%ROLE_SUPER_ADMIN%'")
-            ->andWhere("u.roles LIKE '%ROLE_ADMIN%'")
+            ->where("u.roles LIKE '%_ADMIN%'")
             ->getQuery()
             ->getResult();
     }
