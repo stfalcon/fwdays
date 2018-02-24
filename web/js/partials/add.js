6706b0c55a268ee79bcfc82dd8a9e0a418f3401c
Andrew.Tom <andij.tomchyshyn@stfalcon.com>
2018-02-24 13:36:49 +0200

    hide fb
    re #39507

--- web/js/partials/add.js
+++ web/js/partials/add.js
@@ -286,8 +286,11 @@ $(document).on('click', '.sub-wants-visit-event', function () {
         });
 });
 
-$(document).ready(function () {
+$('[data-testid="dialog_iframe"]').on('load', function() {
+    $(this).removeClass('fb_customer_chat_bounce_in').addClass('fb_customer_chat_bounce_out').hide();
+});
 
+$(document).ready(function () {
     $('#share-ref__facebook').on('click', function () {
         popupwindow('http://www.facebook.com/sharer/sharer.php?u='+$('#ref-input').val(), 'facebook', 500, 350);
     });
