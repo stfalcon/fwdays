b3f10f9b34ea7d79b79441e97768b70a704c8d27
Andrew.Tom <andij.tomchyshyn@stfalcon.com>
2018-03-26 15:27:54 +0300

    mod gulpfile.js

---
+++ web/js/uk/main.js
@@ -0,0 +1,19 @@
+/*
+ Third party
+ */
+//= ../../../node_modules/jquery/dist/jquery.min.js
+//= ../../../node_modules/remodal/dist/remodal.min.js
+//= ../../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
+
+/*
+
+ Custom
+ */
+//= ../partials/js.cookie.js
+//= ../partials/app.js
+//= ../partials/location.js
+//= ../partials/add.js
+//= ../partials/localization.js
+//= ../jquery.validate.min.js
+//= ../additional-methods.min.js
+//= ../validator_localization_uk.js
