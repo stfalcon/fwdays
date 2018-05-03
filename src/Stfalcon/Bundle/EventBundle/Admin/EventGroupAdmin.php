0dca580811779fdc62905890281009d69671b4d4
Andrew.Tom <andij.tomchyshyn@stfalcon.com>
2018-05-03 12:12:53 +0300

    Add group event
    re #41573

---
+++ src/Stfalcon/Bundle/EventBundle/Admin/EventGroupAdmin.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Stfalcon\Bundle\EventBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Stfalcon\Bundle\EventBundle\Entity\Event;
+use Stfalcon\Bundle\EventBundle\Entity\EventGroup;
+
+/**
+ * Class EventGroupAdmin.
+ */
+class EventGroupAdmin extends Admin
+{
+    private $prevEvents = null;
+
+    /**
+     * @param EventGroup $object
+     *
+     * @return mixed|void
+     */
+    public function preUpdate($object)
+    {
+        /** @var Event $event */
+        foreach ($this->prevEvents as $event) {
+            /** @var EventGroup $object */
+            if (!$object->getEvents()->contains($event)) {
+                $event->setGroup(null);
+            }
+        }
+        /** @var Event $event */
+        foreach ($object->getEvents() as $event) {
+            $event->setGroup($object);
+        }
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        /** @var EventGroup $group */
+        $group = $this->getSubject();
+        if ($group->getId()) {
+            $this->prevEvents = clone $group->getEvents();
+        }
+
+        $formMapper
+            ->add('name')
+            ->add('events');
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+            ->add('events');
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('events');
+    }
+}
