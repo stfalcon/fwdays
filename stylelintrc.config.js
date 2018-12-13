a19e98f5cb844e09ac3a619a5ffe636f9aa1e49d
tomchyshyn <andrij.tomchyshyn@stfalcon.com>
2018-12-13 10:27:52 +0200

    Fix fb iframe

--- stylelintrc.config.js
+++ stylelintrc.config.js
@@ -2,7 +2,7 @@ module.exports = {
     "extends": "./node_modules/stylelint-config-standard",
     "rules": {
         "color-hex-case": "upper",
-        "selector-max-id": 0,
+        "selector-max-id": 1,
         "selector-pseudo-element-colon-notation": "single",
         "no-missing-end-of-source-newline": null,
         "color-hex-length": "long",
