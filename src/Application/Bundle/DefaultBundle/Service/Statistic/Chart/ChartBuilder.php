fc07839a412c5c3be37191a1eae375e40d9367ff
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2019-04-17 11:20:20 +0300

    Переробив загальну статистику продажів і додав графік продажів у порівнянні з попередніми івентами (#498)
    * added calendar charts for event statistics
    
    * added forecasting tickets sales analytics
    
    * refactoring. embed charts into AnalyticsController and rendered them as independente actions
    
    * Scrutinizer Auto-Fixes (#500)
    
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Application/Bundle/DefaultBundle/Service/Statistic/Chart/ChartBuilder.php
+++ src/Application/Bundle/DefaultBundle/Service/Statistic/Chart/ChartBuilder.php
@@ -22,17 +22,56 @@ class ChartBuilder
         $this->ticketRepository = $ticketRepository;
     }
 
+    public function columnChart($data) {
+        $chart = new \CMEN\GoogleChartsBundle\GoogleCharts\Charts\Material\ColumnChart();
+        $chart->getData()->setArrayToDataTable($data);
+        $chart->getOptions()->getChart()
+            ->setTitle('Статистика продаж')
+            ->setSubtitle('За последние 30 дней');
+        $chart->getOptions()
+            ->setBars('vertical')
+            ->setHeight(400)
+            ->setWidth(1400)
+//            ->setColors(['#1b9e77', '#d95f02', '#7570b3'])
+            ->getVAxis()
+            ->setFormat('decimal');
+        return $chart;
+    }
+
     /**
-     * @param Event|null $event
+     *
+     *
+     * @param $data
+     * @return \CMEN\GoogleChartsBundle\GoogleCharts\Charts\CalendarChart
+     */
+    public function calendarChart($data) {
+        $chart = new \CMEN\GoogleChartsBundle\GoogleCharts\Charts\CalendarChart();
+        $chart->getData()->setArrayToDataTable($data);
+
+        // рахуєм різницю в роках між першою датою графіка і останньою, щоб динамічно підлаштувати висоту канви
+        $firstDate = array_keys($data)[1]; // в 0-му елементі заголовки графіка. перша дата в 1-му
+        $lastDate = array_keys($data)[count($data)-1];
+
+        $years = (new \DateTime($lastDate))->format('Y') - (new \DateTime($firstDate))->format('Y') + 1;
+        $chart->getOptions()->setHeight(200*$years);
+
+        $chart->getOptions()->getCalendar()->setCellSize(18);
+        $chart->getOptions()->getNoDataPattern()->setBackgroundColor('#76a7fa');
+        $chart->getOptions()->getNoDataPattern()->setColor('#a0c3ff');
+
+        return $chart;
+    }
+
+    /**
+     * @param array $data
      *
      * @return LineChart
+     * @throws \Exception
      */
-    public function buildLineChartForSoldTicketsDuringLastMonth(Event $event = null)
+    public function lineChart($data)
     {
-        $ticketsBoughtDuringLastMonthGroupedByDate = $this->ticketRepository->getBoughtTicketsCountForTheLastGroupedByDateForChart($event);
-        array_unshift($ticketsBoughtDuringLastMonthGroupedByDate, ['Дата', 'Количество билетов']);
         $chart = new LineChart();
-        $chart->getData()->setArrayToDataTable($ticketsBoughtDuringLastMonthGroupedByDate);
+        $chart->getData()->setArrayToDataTable($data);
 
         $chart->getOptions()->setTitle('Продажа билетов за прошедший месяц');
         $chart->getOptions()->setHeight(600);
