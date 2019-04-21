1560a2dd72296c2998a323885b0a9e97367058d5
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2019-04-21 17:35:50 +0300

    rm default news and event news (unused)

---
+++ app/DoctrineMigrations/Version20190421175142.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20190421175142 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE event_news_translations');
+        $this->addSql('DROP TABLE event__news');
+        $this->addSql('DROP TABLE news');
+        $this->addSql('DROP TABLE news_translations');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    }
+}
