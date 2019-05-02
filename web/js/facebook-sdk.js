1fcb000cd043ad25d6c94c79e259083920fe4e38
tomchyshyn <andrij.tomchyshyn@stfalcon.com>
2019-05-02 15:42:24 +0300

    Add chat js

--- web/js/facebook-sdk.js
+++ web/js/facebook-sdk.js
@@ -5,3 +5,11 @@
     js.src = "https://connect.facebook.net/"+lang+"/all.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/"+lang+"/sdk/xfbml.customerchat.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
