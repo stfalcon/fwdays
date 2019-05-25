d876748b7b8bfd0ef1a93a7888c8930015a3503b
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2019-05-25 17:22:59 +0300

    Updated phpunit. Rm unused files and configs
    * master:
      merged with origin/master
      applied patch
    
    # Conflicts:
    #	app/config/services.yml
    #	composer.json
    #	composer.lock
    #	docker-configs/k8s/prod/fwdays-backend.yaml
    #	docker-configs/k8s/stag/fwdays-backend.yaml
    #	src/Application/Bundle/DefaultBundle/Admin/CategoryAdmin.php
    #	src/Application/Bundle/DefaultBundle/Admin/EventAdmin.php
    #	src/Application/Bundle/DefaultBundle/Admin/EventBlockAdmin.php
    #	src/Application/Bundle/DefaultBundle/Admin/PromoCodeAdmin.php
    #	src/Application/Bundle/DefaultBundle/Admin/SpeakerAdmin.php
    #	src/Application/Bundle/DefaultBundle/Admin/SponsorAdmin.php
    #	src/Application/Bundle/DefaultBundle/Admin/UserAdmin.php
    #	src/Application/Bundle/DefaultBundle/Admin/WayForPayLogAdmin.php
    #	src/Application/Bundle/DefaultBundle/Controller/AdminController.php
    #	src/Application/Bundle/DefaultBundle/Controller/AnalyticsController.php
    #	src/Application/Bundle/DefaultBundle/Controller/DefaultController.php
    #	src/Application/Bundle/DefaultBundle/Controller/PaymentController.php
    #	src/Application/Bundle/DefaultBundle/Controller/TrashController.php
    #	src/Application/Bundle/DefaultBundle/DependencyInjection/ApplicationDefaultExtension.php
    #	src/Application/Bundle/DefaultBundle/Entity/EventGroup.php
    #	src/Application/Bundle/DefaultBundle/Entity/Sponsor.php
    #	src/Application/Bundle/DefaultBundle/Entity/TicketCost.php
    #	src/Application/Bundle/DefaultBundle/Features/Context/FeatureContext.php
    #	src/Application/Bundle/DefaultBundle/Form/Type/ProfileFormType.php
    #	src/Application/Bundle/DefaultBundle/Helper/NewPdfGeneratorHelper.php
    #	src/Application/Bundle/DefaultBundle/Model/UserManager.php
    #	src/Application/Bundle/DefaultBundle/Resources/config/services.xml
    #	src/Application/Bundle/DefaultBundle/Resources/config/services.yml
    #	src/Application/Bundle/DefaultBundle/Resources/views/Analytics/comparison_with_previous_events.html.twig
    #	src/Application/Bundle/DefaultBundle/Resources/views/Analytics/daily_dynamics.html.twig
    #	src/Application/Bundle/DefaultBundle/Resources/views/Analytics/forecastedSales.html.twig
    #	src/Application/Bundle/DefaultBundle/Resources/views/Analytics/generalStatistics.html.twig
    #	src/Application/Bundle/DefaultBundle/Resources/views/Default/cabinet.html.twig
    #	src/Application/Bundle/DefaultBundle/Resources/views/Default/page.html.twig
    #	src/Application/Bundle/DefaultBundle/Resources/views/Page/index.html.twig
    #	src/Application/Bundle/DefaultBundle/Resources/views/Redesign/cabinet.html.twig
    #	src/Application/Bundle/DefaultBundle/Resources/views/Redesign/static.page.html.twig
    #	src/Application/Bundle/DefaultBundle/Service/AnalyticsService.php
    #	src/Application/Bundle/DefaultBundle/Service/EventBlock/CandidateSpeakersEventBlockService.php
    #	src/Application/Bundle/DefaultBundle/Service/EventBlock/CommitteeSpeakersEventBlockService.php
    #	src/Application/Bundle/DefaultBundle/Service/EventBlock/TicketStatusEventBlockService.php
    #	src/Application/Bundle/DefaultBundle/Service/FooterBlockService.php
    #	src/Application/Bundle/DefaultBundle/Service/TicketService.php
    #	src/Application/Bundle/DefaultBundle/Tests/Controller/WayForPayControllerTest.php
    #	src/Application/Bundle/DefaultBundle/Tests/Service/PaymentServiceTest.php
    #	src/Application/Bundle/DefaultBundle/Tests/Service/ReferralServiceTest.php
    #	src/Application/Bundle/UserBundle/Controller/DefaultController.php
    #	src/Application/Bundle/UserBundle/Resources/views/Default/cabinet.html.twig
    #	src/Stfalcon/Bundle/EventBundle/Repository/PromoCodeRepository.php
    #	src/Stfalcon/Bundle/EventBundle/Resources/config/services.xml
    #	web/js/jquery-1.8.2.min.js

--- web/app_test.php
+++ web/app_test.php
@@ -1,19 +1,21 @@
 <?php
 
-// this check prevents access to debug front controllers that are deployed by accident to production servers.
-// feel free to remove this, extend it, or make something more sophisticated.
-if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
-    '127.0.0.1',
-    '::1',
-))) {
+// This check prevents access to debug front controllers that are deployed by accident to production servers.
+// Feel free to remove this, extend it, or make something more sophisticated.
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
+) {
     header('HTTP/1.0 403 Forbidden');
-    die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
-
-use Symfony\Component\HttpFoundation\Request;
+require __DIR__.'/../vendor/autoload.php';
+Debug::enable();
 
 $kernel = new AppKernel('test', true);
-$kernel->handle(Request::createFromGlobals())->send();
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
