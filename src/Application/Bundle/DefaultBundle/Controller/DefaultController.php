fa0be1d68a6172991d17742da80ff90849ac7d7e
Andrij Tomchyshyn <andrij.tomchyshyn@stfalcon.com>
2019-08-08 11:13:49 +0300

    fix contact page.

--- src/Application/Bundle/DefaultBundle/Controller/DefaultController.php
+++ src/Application/Bundle/DefaultBundle/Controller/DefaultController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\ConstraintViolation;
 
 /**
