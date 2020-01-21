5d0b9a097ec7d23be1e92cbffcf4c4e2b1f10a13
Andrij Tomchyshyn <andrij.tomchyshyn@stfalcon.com>
2020-01-21 15:31:53 +0200

    Add crud rules.

--- src/AppBundle/Admin/PromoCodeAdmin.php
+++ src/AppBundle/Admin/PromoCodeAdmin.php
@@ -4,6 +4,8 @@ namespace App\Admin;
 
 use App\Admin\AbstractClass\AbstractTranslateAdmin;
 use App\Entity\Event;
+use App\Entity\PromoCode;
+use App\Entity\User;
 use App\Form\Type\MyGedmoTranslationsType;
 use App\Service\LocalsRequiredService;
 use Doctrine\Common\Collections\Criteria;
@@ -12,12 +14,58 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * Class PromoCodeAdmin.
  */
 class PromoCodeAdmin extends AbstractTranslateAdmin
 {
+    /**
+     * Allows you to customize batch actions.
+     *
+     * @param array $actions List of actions
+     *
+     * @return array
+     */
+    protected function configureBatchActions($actions): array
+    {
+        $user = $this->getCurrentUser();
+        if (!$user instanceof User || !\in_array('ROLE_SUPER_ADMIN', $user->getRoles(), true)) {
+            unset($actions['delete']);
+        }
+
+        return $actions;
+    }
+
+    /**
+     * @return array
+     */
+    public function getBatchActions()
+    {
+        $user = $this->getCurrentUser();
+        $isSuperAdmin = $user instanceof User && \in_array('ROLE_SUPER_ADMIN', $user->getRoles(), true);
+
+        if (!$isSuperAdmin) {
+            return [];
+        }
+
+        return parent::getBatchActions();
+    }
+
+    /**
+     * @param object $object
+     */
+    public function preRemove($object)
+    {
+        $user = $this->getCurrentUser();
+
+        if (!$user instanceof User || !\in_array('ROLE_SUPER_ADMIN', $user->getRoles(), true)) {
+           throw new AccessDeniedException();
+        }
+    }
+
     /**
      * @var array
      */
@@ -51,6 +99,18 @@ class PromoCodeAdmin extends AbstractTranslateAdmin
     {
         $localsRequiredService = $this->getConfigurationPool()->getContainer()->get(LocalsRequiredService::class);
         $localOptions = $localsRequiredService->getLocalsRequiredArray();
+        /** @var PromoCode|null $promocode */
+        $promocode = $this->getSubject();
+        $user = $this->getCurrentUser();
+        $isSuperAdmin = $user instanceof User && \in_array('ROLE_SUPER_ADMIN', $user->getRoles(), true);
+        $creator = null;
+        $allowedToEdit = false;
+        if ($promocode instanceof PromoCode) {
+            $creator = $promocode->getCreatedBy();
+            $isCreator = $user instanceof User && $creator instanceof User && $user->isEqualTo($creator);
+            $allowedToEdit = ($isCreator && 0 === $promocode->getUsedCount()) || $isSuperAdmin || null === $promocode->getId();
+        }
+
         $datetimePickerOptions =
             [
                 'format' => 'dd.MM.y',
@@ -60,6 +120,7 @@ class PromoCodeAdmin extends AbstractTranslateAdmin
                 ->add('translations', MyGedmoTranslationsType::class, [
                     'label' => 'Переводы',
                     'translatable_class' => $this->getClass(),
+                    'disabled' => !$allowedToEdit,
                     'fields' => [
                         'title' => [
                             'label' => 'Название',
@@ -69,26 +130,28 @@ class PromoCodeAdmin extends AbstractTranslateAdmin
                 ])
             ->end()
             ->with('Общие')
-                ->add('discountAmount', null, ['required' => true, 'label' => 'Скидка (%)'])
-                ->add('code', null, ['label' => 'Код'])
+                ->add('discountAmount', null, ['required' => true, 'disabled' => !$allowedToEdit, 'label' => 'Скидка (%)'])
+                ->add('code', null, ['disabled' => !$allowedToEdit, 'label' => 'Код'])
                 ->add('event', null, [
                     'label' => 'Событие',
                     'required' => true,
+                    'disabled' => !$allowedToEdit,
                     'placeholder' => 'Choose event',
                 ])
-                ->add('maxUseCount', null, ['label' => 'Максимальное количество использований', 'help' => '(0 - безлимитный)'])
+                ->add('maxUseCount', null, ['disabled' => !$allowedToEdit, 'label' => 'Максимальное количество использований', 'help' => '(0 - безлимитный)'])
                 ->add(
                     'endDate',
                     'sonata_type_date_picker',
                     array_merge(
                         [
+                            'disabled' => !$allowedToEdit,
                             'required' => true,
                             'label' => 'Дата окончания',
                         ],
                         $datetimePickerOptions
                     )
                 )
-                ->add('description', TextType::class, ['label' => 'Описание', 'required' => false])
+                ->add('description', TextType::class, ['disabled' => !$allowedToEdit, 'label' => 'Описание', 'required' => false])
             ->end();
     }
 
@@ -116,5 +179,26 @@ class PromoCodeAdmin extends AbstractTranslateAdmin
         $eventRepository = $this->getConfigurationPool()->getContainer()->get('doctrine')->getRepository(Event::class);
 
         return $eventRepository->findBy([], ['id' => Criteria::DESC]);
+    }
+
+    /**
+     * @return User|null
+     */
+    private function getCurrentUser(): ?User
+    {
+        $token = $this->getConfigurationPool()->getContainer()->get('security.token_storage')->getToken();
+
+        if (!$token instanceof TokenInterface) {
+            return null;
+        }
+
+        /** @var User $user */
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            throw new AccessDeniedException();
+        }
+
+        return $user;
     }
 }
