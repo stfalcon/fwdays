555cfdbc1177107acf0f0e23d1658ab6fab4c1c6
Andrij Tomchyshyn <andrij.tomchyshyn@stfalcon.com>
2020-02-26 10:27:29 +0200

    Fix tests, translates.

--- src/Admin/PaymentAdmin.php
+++ src/Admin/PaymentAdmin.php
@@ -13,7 +13,6 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\Form\Type\CollectionType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
