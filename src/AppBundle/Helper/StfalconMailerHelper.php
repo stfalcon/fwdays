f3829ffb1c6b829b59b4eb500e4a10d451a4c871
andrij.tomchyshyn@stfalcon.com <3X9LNQhQE4AH9iZE>
2020-03-27 17:53:31 +0200

    Fix empty line import.

--- src/AppBundle/Helper/StfalconMailerHelper.php
+++ src/AppBundle/Helper/StfalconMailerHelper.php
@@ -2,10 +2,12 @@
 
 namespace App\Helper;
 
-use App\Entity\Mail;
+use App\Entity\Ticket;
 use App\Entity\User;
 use App\Model\TranslatedMail;
 use Doctrine\ORM\EntityManager;
+use Endroid\QrCode\Exceptions\ImageFunctionFailedException;
+use Endroid\QrCode\Exceptions\ImageFunctionUnknownException;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Component\Translation\TranslatorInterface;
 use Twig\Environment;
@@ -20,21 +22,25 @@ class StfalconMailerHelper
     protected $router;
     protected $mailer;
     private $translator;
+    /** @var NewPdfGeneratorHelper $pdfGeneratorHelper */
+    private $pdfGeneratorHelper;
 
     /**
-     * @param Environment         $twig
-     * @param EntityManager       $em
-     * @param Router              $router
-     * @param \Swift_Mailer       $mailer
-     * @param TranslatorInterface $translator
+     * @param Environment           $twig
+     * @param EntityManager         $em
+     * @param Router                $router
+     * @param \Swift_Mailer         $mailer
+     * @param TranslatorInterface   $translator
+     * @param NewPdfGeneratorHelper $pdfGeneratorHelper
      */
-    public function __construct(Environment $twig, EntityManager $em, Router $router, \Swift_Mailer $mailer, TranslatorInterface $translator)
+    public function __construct(Environment $twig, EntityManager $em, Router $router, \Swift_Mailer $mailer, TranslatorInterface $translator, NewPdfGeneratorHelper $pdfGeneratorHelper)
     {
         $this->twig = $twig;
         $this->em = $em;
         $this->router = $router;
         $this->mailer = $mailer;
         $this->translator = $translator;
+        $this->pdfGeneratorHelper = $pdfGeneratorHelper;
     }
 
     /**
@@ -43,32 +49,25 @@ class StfalconMailerHelper
      * @param User           $user          User
      * @param TranslatedMail $mail          Mail
      * @param bool           $isTestMessage Test message (needed for admin mails)
-     * @param bool           $withTicket
      *
      * @return \Swift_Message
      */
-    public function formatMessage(User $user, TranslatedMail $mail, $isTestMessage = false, $withTicket = false): \Swift_Message
+    public function formatMessage(User $user, TranslatedMail $mail, $isTestMessage = false): \Swift_Message
     {
-        if ($withTicket) {
-            $event = $mail->getEvents()[0] ?? null;
-            $params = ['event' => $event];
-            $template = '@App/Email/email_with_ticket.html.twig';
-        } else {
-            $text = $this->replace(
-                $mail->getText(),
-                [
-                    '%fullname%' => $user->getFullname(),
-                    '%user_id%' => $user->getId(),
-                ]
-            );
-            $template = '@App/Email/new_email.html.twig';
-            $params =
-                [
-                    'text' => $text,
-                    'mail' => $mail,
-                    'user' => $user,
-                ];
-        }
+        $text = $this->replace(
+            $mail->getText(),
+            [
+                '%fullname%' => $user->getFullname(),
+                '%user_id%' => $user->getId(),
+            ]
+        );
+        $template = '@App/Email/new_email.html.twig';
+        $params =
+            [
+                'text' => $text,
+                'mail' => $mail,
+                'user' => $user,
+            ];
 
         $body = $this->renderTwigTemplate($template, $params);
 
@@ -79,6 +78,39 @@ class StfalconMailerHelper
         }
 
         return $this->createMessage($title, $user->getEmail(), $body);
+    }
+
+    /**
+     * @param Ticket $ticket
+     *
+     * @return \Swift_Message
+     *
+     * @throws ImageFunctionFailedException
+     * @throws ImageFunctionUnknownException
+     */
+    public function formatMessageWithTicket(Ticket $ticket)
+    {
+        $event = $ticket->getEvent();
+        $user = $ticket->getUser();
+
+        $body = $this->renderTwigTemplate('@App/Email/email_with_ticket.html.twig',
+            [
+                'event' => $event,
+                'ticket' => $ticket,
+                'user' => $user,
+            ]);
+
+        $message = $this->createMessage($event->getName(), $user->getEmail(), $body);
+
+        $html = $this->pdfGeneratorHelper->generateHTML($ticket);
+        $message->attach(
+            \Swift_Attachment::newInstance(
+                $this->pdfGeneratorHelper->generatePdfFile($ticket, $html),
+                $ticket->generatePdfFilename()
+            )
+        );
+
+        return $message;
     }
 
     /**
