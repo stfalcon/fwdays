18686f95eaf508043b604ae5c2342113b3c6b5aa
andrij.tomchyshyn@stfalcon.com <3X9LNQhQE4AH9iZE>
2020-06-04 17:35:03 +0300

    Add refers to payment list.

--- src/Repository/Referer/RefererRepository.php
+++ src/Repository/Referer/RefererRepository.php
@@ -3,7 +3,11 @@
 namespace App\Repository\Referer;
 
 use App\Entity\Referer\Referer;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\ORM\Query\Parameter;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -30,6 +34,31 @@ class RefererRepository extends ServiceEntityRepository
         $qb->where($qb->expr()->isNull('r.user'))
             ->andWhere($qb->expr()->eq('r.cookieId', ':cookieId'))
             ->setParameter('cookieId', $cookieId)
+        ;
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @param User      $user
+     * @param \DateTime $dateTime
+     *
+     * @return Referer[]
+     */
+    public function findAllByUserBeforeDate(User $user, \DateTime $dateTime)
+    {
+        $qb = $this->createQueryBuilder('r');
+        $qb->where($qb->expr()->eq('r.user', ':user'))
+            ->andWhere($qb->expr()->lt('r.date', ':date'))
+            ->setParameters(new ArrayCollection(
+                    [
+                        new Parameter('user', $user),
+                        new Parameter('date', $dateTime),
+                    ]
+                )
+            )
+            ->setMaxResults(10)
+            ->orderBy('r.date', Criteria::DESC)
         ;
 
         return $qb->getQuery()->getResult();
